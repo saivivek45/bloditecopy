f79cab1374ce98652b1ab0a0123faf6b885498fd
Pratik Raj <pratikraj220011@gmail.com>
2025-07-25 20:41:32 +0530

    feat: add schemas for updating password and profile with validation rules

---
+++ src/schema/updatePasswordSchema.ts
@@ -0,0 +1,19 @@
+import { z } from 'zod';
+
+export const updatePasswordSchema = z.object({
+
+  currentPassword: z
+  .string({message: "Invalid Input"})
+  .regex(/^(?=.*[A-Z])(?=.*\d).*$/, {message: "Password must have one uppercase and digit"})
+  .min(8, {message: "Password should be atleast 8 characters"}),
+
+  newPassword: z
+  .string({message: "Invalid Input"})
+  .regex(/^(?=.*[A-Z])(?=.*\d).*$/, {message: "Password must have one uppercase and digit"})
+  .min(8, {message: "Password should be atleast 8 characters"}),
+
+  confimrPassword: z
+  .string({message: "Invalid Input"})
+  .regex(/^(?=.*[A-Z])(?=.*\d).*$/, {message: "Password must have one uppercase and digit"})
+  .min(8, {message: "Password should be atleast 8 characters"})
+})
---
+++ src/schema/updateProfileSchema.ts
@@ -0,0 +1,11 @@
+import { z } from 'zod';
+
+export const updateProfileSchema = z.object({
+  email: z
+  .string({message: "Invalid Input"})
+  .email({message: "Invalid email address"}),
+
+  name: z
+  .string({message: "Invalid Input"})
+  .min(1, {message: "Password should be atleast 8 characters"})
+})
